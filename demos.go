bb0a22871887ebadba51228159eea85df843376c
Ivan Voras <ivoras@gmail.com>
2020-04-05 18:46:05 +0200

    Initial version

--- demos.go
+++ demos.go
@@ -4,33 +4,47 @@ import (
 	"math/rand"
 )
 
+/*
+ * Spread probability = probability of infection for each encounter between 2 people.
+ */
+
+const AlgorithmTypeDefault = 0
+const AlgorithmTypeFaster = 1
+
 type DiseaseParameters struct {
-	PopulationCount         uint32  `json:"population_count"`
+	AlgorithmType           uint32  `json:"algorithm_type"`
+	PopulationCount         uint32  `json:"population_count"`                       // Population size
+	StartInfected           uint32  `json:"start_infected"`                         // Count of initially infected people
+	CollapseThreshold       uint32  `json:"collapse_threshold"`                     // Health system collapse threshold - collapses if this many people are ill
 	InteractionCircleCount  uint32  `json:"interaction_circle_count"`               // Dumbar's number
 	AlwaysAsymptomaticRatio float32 `json:"asymptomatic_ratio"`                     // Percentage of people who are always asymptomatic
 	IsolationViolatorsRatio float32 `json:"isolation_violators_ratio"`              // Percentage of people who will ignore isolation rules
 	AsymptomaticDays        uint32  `json:"asymptomatic_days"`                      // Number of days at which those who are not asymptomatic, look asymptomatic
-	RIsolationDay           float32 `json:"spread_rate_isolation"`                  // Spread rate per day of non-asymptomatic people in isolation
-	RNotIsolationDay        float32 `json:"spread_rate_not_isolation"`              // Spread rate per day of non-asympromatic people not in isolation
-	RAIsolationDay          float32 `json:"spread_rate_asymptomatic_isolation"`     // Spread rate per day for always asymptomatic people in isolation
-	RANotIsolationDay       float32 `json:"spread_rate_asymptomatic_not_isolation"` // Spread rate per day for always asymptomatic people not in isolation
-	//	AgeInfectionRatioBase   float32 `json:"age_infection_rate_base"`                // Baseline (minimal) infection rate added to 1/(100-x)
-	RDeathNormal   float32 `json:"death_rate_normal"`   // Death rate with a functioning healthcare system
-	RDeathCollapse float32 `json:"death_rate_collapse"` // Death rate without a functioning healthcare system
+	TotalDiseaseDays        uint32  `json:"total_disease_days"`                     // Total days the disease is present in a person
+	RIsolationProb          float32 `json:"spread_rate_isolation"`                  // Spread probability of non-asymptomatic people in isolation
+	RNotIsolationProb       float32 `json:"spread_rate_not_isolation"`              // Spread probability of non-asympromatic people not in isolation
+	RAIsolationProb         float32 `json:"spread_rate_asymptomatic_isolation"`     // Spread probability for always asymptomatic people in isolation
+	RANotIsolationProb      float32 `json:"spread_rate_asymptomatic_not_isolation"` // Spread probability for always asymptomatic people not in isolation
+	RDeathNormal            float32 `json:"death_rate_normal"`                      // Death rate with a functioning healthcare system
+	RDeathCollapse          float32 `json:"death_rate_collapse"`                    // Death rate without a functioning healthcare system
 }
 
 var defaultParams = DiseaseParameters{
+	AlgorithmType:           AlgorithmTypeDefault,
 	PopulationCount:         10_000_000,
-	InteractionCircleCount:  100,
+	CollapseThreshold:       5000,
+	StartInfected:           1000,
+	InteractionCircleCount:  40,
 	AlwaysAsymptomaticRatio: 0.5,
 	IsolationViolatorsRatio: 0.1,
-	AsymptomaticDays:        14,
-	RIsolationDay:           0.001,
-	RNotIsolationDay:        2,
-	RAIsolationDay:          0.0001,
-	RANotIsolationDay:       0.001,
-	RDeathNormal:            0.02,
-	RDeathCollapse:          0.1,
+	AsymptomaticDays:        13,
+	TotalDiseaseDays:        25,
+	RIsolationProb:          0.001,
+	RNotIsolationProb:       0.4,
+	RAIsolationProb:         0.0001,
+	RANotIsolationProb:      0.001,
+	RDeathNormal:            0.0008, // 0.02 / 25
+	RDeathCollapse:          0.005,  // 0.13 / 25
 }
 
 /*
@@ -42,12 +56,14 @@ func (dp DiseaseParameters) AgeInfectionRate(age byte) float32 {
 const PERSON_STATUS_ALIVE = 1                // Person is alive
 const PERSON_STATUS_INFECTED = 2             // Person is infected
 const PERSON_STATUS_IN_ISOLATION = 4         // Person in isolation
-const PERSON_STATUS_ASYMPTOMATIC = 8         // Person is asymptomatic
+const PERSON_STATUS_SYMPTOMATIC = 8          // Person is asymptomatic
 const PERSON_STATUS_ALWAYS_ASYMPTOMATIC = 16 // Person is always asymptomatic
 const PERSON_STATUS_ISOLATION_VIOLATOR = 32  // Person is an isolation violator
+const PERSON_STATUS_IMMUNE = 64              // Person is immune (e.g. from previous illness)
 
 type Person struct {
-	Status byte
+	Status       byte
+	DaysInfected byte
 }
 
 func (p Person) IsAlive() bool {
@@ -62,8 +78,8 @@ func (p Person) IsInIsolation() bool {
 	return p.Status&PERSON_STATUS_IN_ISOLATION != 0
 }
 
-func (p Person) IsAsymptompatic() bool {
-	return p.Status&PERSON_STATUS_ASYMPTOMATIC != 0
+func (p Person) IsSymptomatic() bool {
+	return p.Status&PERSON_STATUS_SYMPTOMATIC != 0
 }
 
 func (p Person) IsAlwaysAsymptomatic() bool {
@@ -72,6 +88,10 @@ func (p Person) IsAlwaysAsymptomatic() bool {
 
 func (p Person) IsIsolationViolator() bool {
 	return p.Status&PERSON_STATUS_ISOLATION_VIOLATOR != 0
+}
+
+func (p Person) IsImmune() bool {
+	return p.Status&PERSON_STATUS_IMMUNE != 0
 }
 
 type World struct {
@@ -83,12 +103,17 @@ func NewWorld(dp DiseaseParameters) (w World) {
 	w.dParams = dp
 	w.Population = make([]Person, dp.PopulationCount)
 	for i := range w.Population {
+		status := byte(PERSON_STATUS_ALIVE)
 		if rand.Float32() < dp.AlwaysAsymptomaticRatio {
-			w.Population[i].Status = PERSON_STATUS_ALWAYS_ASYMPTOMATIC
+			status |= PERSON_STATUS_ALWAYS_ASYMPTOMATIC
 		}
 		if rand.Float32() < dp.IsolationViolatorsRatio {
-			w.Population[i].Status ^= PERSON_STATUS_ISOLATION_VIOLATOR
+			status |= PERSON_STATUS_ISOLATION_VIOLATOR
 		}
+		if uint32(i) < w.dParams.StartInfected {
+			status |= PERSON_STATUS_INFECTED
+		}
+		w.Population[i].Status = status
 	}
 	return
 }
@@ -102,21 +127,93 @@ func (w *World) DeadCount() (count uint32) {
 	return
 }
 
-func (w *World) TryInfect(src, tgt *Person) {
-	tgt.Status ^= PERSON_STATUS_INFECTED
+func (w *World) TryInfect(src Person, tgt *Person) {
+	if tgt.IsImmune() {
+		return
+	}
+	prob := float32(0)
+	if !src.IsSymptomatic() && !src.IsInIsolation() {
+		prob = w.dParams.RANotIsolationProb
+	} else if !src.IsSymptomatic() && src.IsInIsolation() {
+		prob = w.dParams.RAIsolationProb
+	} else if src.IsSymptomatic() && !src.IsInIsolation() {
+		prob = w.dParams.RNotIsolationProb
+	} else if src.IsSymptomatic() && src.IsInIsolation() {
+		prob = w.dParams.RIsolationProb
+	}
+	if rand.Float32() < prob {
+		tgt.Status |= PERSON_STATUS_INFECTED
+		if !tgt.IsIsolationViolator() {
+			tgt.Status |= PERSON_STATUS_IN_ISOLATION
+		}
+	}
 }
 
 func (w *World) NewDay() {
-	// Process new infections
+	// Calculate if the health system has collapsed
+	nInfected := uint32(0)
+	for _, p := range w.Population {
+		if p.IsInfected() && p.IsAlive() {
+			nInfected++
+		}
+	}
+	rDeath := w.dParams.RDeathNormal
+	if nInfected > w.dParams.CollapseThreshold {
+		rDeath = w.dParams.RDeathCollapse
+	}
+	// Process each person
 	for i, p := range w.Population {
 		if p.IsAlive() && p.IsInfected() {
+			if uint32(p.DaysInfected) >= w.dParams.TotalDiseaseDays {
+				w.Population[i].Status &^= PERSON_STATUS_INFECTED
+				w.Population[i].Status &^= PERSON_STATUS_IN_ISOLATION
+				w.Population[i].Status |= PERSON_STATUS_IMMUNE
+				continue
+			}
+			if rand.Float32() < rDeath {
+				w.Population[i].Status &^= PERSON_STATUS_ALIVE
+				continue
+			}
+			w.Population[i].DaysInfected++
+			if uint32(w.Population[i].DaysInfected) > w.dParams.AsymptomaticDays && !p.IsAlwaysAsymptomatic() {
+				w.Population[i].Status |= PERSON_STATUS_SYMPTOMATIC
+			}
 			// Assumption: a person always has contact with the same InteractionCircleCount people
+			p = w.Population[i]
 			rSource := rand.NewSource(int64(i))
 			r := rand.New(rSource)
 			for j := uint32(0); j < w.dParams.InteractionCircleCount; j++ {
 				tgt := r.Intn(int(w.dParams.PopulationCount))
-				w.TryInfect(&p, &w.Population[tgt])
+				w.TryInfect(p, &w.Population[tgt])
 			}
 		}
 	}
+}
+
+type WorldStat struct {
+	LiveCount      int32  `json:"live_count"`
+	InfectedCount  uint32 `json:"infected_count"`
+	DeadCount      uint32 `json:"dead_count"`
+	IsolationCount uint32 `json:"isolation_count"`
+	ImmuneCount    uint32 `json:"immune_count"`
+}
+
+func (w World) GetStat() (st WorldStat) {
+	for _, p := range w.Population {
+		if p.IsAlive() {
+			st.LiveCount++
+			if p.IsInfected() {
+				st.InfectedCount++
+			}
+			if p.IsInIsolation() {
+				st.IsolationCount++
+			}
+			if p.IsImmune() {
+				st.ImmuneCount++
+			}
+		} else {
+			st.DeadCount++
+		}
+	}
+	return
 }
--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+)
+
+var isPrint = flag.Bool("p", false, "Prints the default values as JSON and exits")
+var loadFile = flag.String("l", "", "Loads parameters from JSON file (see output of -p")
+
 func main() {
-	
+	flag.Parse()
+
+	if *isPrint {
+		param, err := json.MarshalIndent(defaultParams, "", "  ")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", string(param))
+
+	}
+
+	params := defaultParams
+
+	if *loadFile != "" {
+		data, err := ioutil.ReadFile(*loadFile)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = json.Unmarshal(data, &params)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	jsonParam, err := json.Marshal(params)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("; %s\n", string(jsonParam))
+	fmt.Printf("day, live_count, infected_count, dead_count, isolation_count, immune_count\n")
+	w := NewWorld(params)
+	for i := 0; ; i++ {
+		st := w.GetStat()
+		fmt.Printf("%4d, %06d, %6d, %6d, %6d, %6d\n", i, st.LiveCount, st.InfectedCount, st.DeadCount, st.IsolationCount, st.ImmuneCount)
+		if st.InfectedCount == 0 {
+			break
+		}
+		w.NewDay()
+	}
 }
